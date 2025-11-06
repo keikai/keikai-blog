3ac9b0fdaa53a3dd4a72d9fd8752473bf95298b2
mduchemin <matthieu@potix.com>
2025-11-06 16:00:14 +0100

    load ga4 update

--- blog/js/main.js
+++ blog/js/main.js
@@ -21,18 +21,6 @@
 }
 )(window);
 
-/*Google Page Tracker*/
-//https://developers.google.com/analytics/devguides/collection/analyticsjs/creating-trackers
-//https://developers.google.com/analytics/devguides/collection/analyticsjs/pages
-//instantiation!
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-116749276-1', 'auto');
-ga('send', 'pageview');
-
 function initFooterResponsive () {
     var toggleClass = function (element, name, add) {
         if (!element) {
